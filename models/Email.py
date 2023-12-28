bda3e0f6f1a6cb2b2a1e8417c2d5a63b9e916c3d
ea-boahen <ea.boahen2015@gmail.com>
2023-12-28 20:56:05 +0000

    push console.py

--- models/Email.py
+++ models/Email.py
@@ -8,18 +8,18 @@ from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
-class City(BaseModel, Base):
+class Person(BaseModel, Base):
     """Representation of city """
     if models.storage_t == "db":
         __tablename__ = 'cities'
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        contact_id = Column(String(60), primary_key=True)
         name = Column(String(128), nullable=False)
-        places = relationship("Place",
-                              backref="cities",
-                              cascade="all, delete, delete-orphan")
+        contact = Column(String(128), nullable=False)
+
     else:
-        state_id = ""
+        Contact_id = ""
         name = ""
+        contact = ""
 
     def __init__(self, *args, **kwargs):
         """initializes city"""
